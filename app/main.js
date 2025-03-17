bc9d4390c3da731b2dd1f24793f47ab9bbff8c78
merrick2000 <menongbemerrick@gmail.com>
2025-03-17 09:51:13 +0100

    implement REPL

--- app/main.js
+++ app/main.js
@@ -6,7 +6,28 @@ const rl = readline.createInterface({
 });
 
 // Uncomment this block to pass the first stage
-rl.question("$ ", (answer) => {
-  console.log(`${answer}: command not found`)
+// rl.question("$ ", (answer) => {
+//   console.log(`${answer}: command not found`)
+//   rl.close();
+// });
+
+function prompt() {
+  rl.question("$ ", (answer) => {
+    if (answer.trim() === "exit") {
+      rl.close();
+      return;
+    }
+
+    console.log(`${answer}: command not found`);
+    // On reboucle pour attendre la prochaine commande
+    prompt();
+  });
+}
+
+// Démarrer la boucle REPL
+prompt();
+
+// Gérer Ctrl+C proprement
+rl.on('SIGINT', () => {
   rl.close();
 });
